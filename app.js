a6217937ba09d7c549d47d428cdc669d31dd6a63
Andrew <aniemann1@gmail.com>
2021-05-01 19:43:10 -0400

    adding more structure

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 //reqs
-const fs = require('fs');
-const conf = require('./config.json');
-const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 
-//init
+//init discord
+const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+//organize commands
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Successfully launched.');
 });
 
 client.on('message', message => {
-	if (message.content === `!ping`) {
-        message.channel.send('Pong!');
-    } else if (message.content === `!ainfo`) {
-        message.channel.send(`Server name:  ${message.guild.name}`);
-    }
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if (!client.commands.has(commandName)) return; 
+        const command = client.commands.get(commandName);
+        if (command.args && !args.length) {
+            let reply = `You didn't provide any arguments, ${message.author}!`;
+            if (command.usage) {
+                reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+            }
+            
+            return message.channel.send(reply)
+        }
+        try {
+	        command.execute(message, args);
+        } catch (error) {
+	        console.error(error);
+	        message.reply('there was an error trying to execute that command!');
+}
 });
 
-client.login(conf.token);
+client.login(token);
