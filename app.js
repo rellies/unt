9a45f384c51712643cad0a0047595a931e828ed7
Andrew <aniemann1@gmail.com>
2021-05-06 22:14:56 -0400

    add reaction client

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
-//reqs
+/* Prerequisites **
+** Imports, variables, declarations */
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
-//init discord
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-
 //organize commands
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+/* Message client block **
+** Handles commands, etc */   
+//init discord
+const messageClient = new Discord.Client();
+const reactionClient = new Discord.Client();
+messageClient.commands = new Discord.Collection();
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+	messageClient.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-	console.log('Awaiting messages...');
-});
-
-//await message input
-    
-client.on('message', message => {
+messageClient.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(commandName)) return; 
-        const command = client.commands.get(commandName);
+	if (!messageClient.commands.has(commandName)) return; 
+        const command = messageClient.commands.get(commandName);
         if (command.args && !args.length) {
             let reply = `You didn't provide any arguments, ${message.author}!`;
             if (command.usage) {
@@ -45,7 +43,37 @@ client.on('message', message => {
 }
 });
 
+/* Reaction block **
+** Handles reactions */
+const { emoji } = require('./config.json');
+
+const filter = (reaction, user) => {
+	return reaction.emoji.name === emoji && user.id === message.author.id;
+};
+
+reactionClient.on('message', async (message) => {
+    message.awaitReactions(filter, { max: 4, time: 60000, errors: ['time'] })
+	.then(collected => console.log(collected.size))
+	.catch(collected => {
+		console.log(console.error);
+	});
+
+});
+
+/* Verification */
 console.log('Logging in...');
-client.login(token);
+messageClient.login(token);
 console.log('Successfully logged in.');
+
+messageClient.once('ready', () => {
+	console.log('Messages client successfully launched.');
+});
+
+/* this is probably fucking useless
+reactionClient.once('ready', () => {
+	console.log('Reactions client successfully launched.');
+}); */
+
+// Verify Reactions
+
 console.log('Successfully launched.');
